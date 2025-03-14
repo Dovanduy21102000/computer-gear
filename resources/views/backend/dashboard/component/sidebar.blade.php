a06eef004b8eb677b60a1cda9deed1fbc4ea80d0
ngoduc204 <ngogiaduc64@gmail.com>
2025-03-14 21:58:13 +0700

    luu thay doi sliderbar

--- resources/views/backend/dashboard/component/sidebar.blade.php
+++ resources/views/backend/dashboard/component/sidebar.blade.php
@@ -10,23 +10,12 @@
             </a>
         </li><!-- End Dashboard Nav -->
         <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#promo-nav" data-bs-toggle="collapse" href="#">
-                <i class="bi bi-layout-text-window-reverse"></i><span>Quản lý khuyến mại</span><i
-                    class="bi bi-chevron-down ms-auto"></i>
+            <a class="nav-link collapsed"  href="{{ route('coupons.index') }}">
+                <i class="bi bi-layout-text-window-reverse"></i><span>Quản lý khuyến mại</span>
             </a>
-            <ul id="promo-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
-                <li>
-                    <a href="{{ route('coupons.index') }}">
-                        <i class="bi bi-circle"></i><span>Danh sách khuyến mại</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('coupons.create') }}">
-                        <i class="bi bi-circle"></i><span>Thêm khuyến mại</span>
-                    </a>
-                </li>
-            </ul>
+            
         </li>
+
         <li class="nav-item">
             <a class="nav-link collapsed" href="{{ route('banners.index') }}">
                 <i class="bi bi-aspect-ratio"></i>
