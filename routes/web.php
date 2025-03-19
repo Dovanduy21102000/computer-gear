f1f0a7e53410300361c56b883842615eb24bed38
Dovanduy21102000 <doduy21102000@gmail.com>
2025-03-19 20:04:03 +0700

    conflict merge product clinet

--- routes/web.php
+++ routes/web.php
@@ -43,28 +43,17 @@ Route::prefix('admin')->group(function () {
 
     Route::post('posts/upload', [PostController::class, 'upload'])->name('posts.upload');
 });
-<<<<<<< HEAD
-=======
-
-Route::resource('users', UserController::class);
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-
-
->>>>>>> acf26489b6efda40ac4b8213f9c61158c0f1f2af
 
 //Client 
 
 // Trang chủ client
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
-<<<<<<< HEAD
-Route::get('/products', [ProductClientController::class, 'index']);
-=======
+
 
 Route::get('/products', [ProductClientController::class, 'index'])->name('client.products.index');
 Route::get('/product/{slug}', [ProductClientController::class, 'show'])->name('client.products.detail');
 
 
->>>>>>> acf26489b6efda40ac4b8213f9c61158c0f1f2af
 Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
 
 Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
