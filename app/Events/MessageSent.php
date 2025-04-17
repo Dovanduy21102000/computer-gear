f751f0059f886a48d78de18927844dbf8ece5fd2
ndh0104 <ndh012004@gmail.com>
2025-04-18 01:57:32 +0700

    Chat realtime

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -11,36 +11,28 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent
+class MessageSent implements ShouldBroadcast
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    /**
-     * Create a new event instance.
-     */
-
     public $message;
+
     public function __construct(Message $message)
     {
         $this->message = $message;
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
     public function broadcastOn()
     {
-        return new PrivateChannel('chat.' . $this->message->receiver_id);
+        // Người nhận sẽ nhận event
+        $receiver = $this->message->receiver_id ?? $this->message->sender_guest_id;
+        return new PrivateChannel("chat.{$receiver}");
     }
+
     public function broadcastWith()
     {
         return [
             'message' => $this->message->message,
             'sender_id' => $this->message->sender_id,
-            'receiver_id' => $this->message->receiver_id,
-            'created_at' => $this->message->created_at->toDateTimeString(),
+            'created_at' => $this->message->created_at->toDateTimeString()
         ];
     }
 }
