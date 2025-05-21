878d8367519252d1cd8093e304c0d94c3779466c
hiennvph46597 <hiennvph46597@fpt.edu.vn>
2025-05-21 20:59:10 +0700

    fix lỗi nhỏ

--- resources/views/fontend/component/footer.blade.php
+++ resources/views/fontend/component/footer.blade.php
@@ -5,158 +5,6 @@
              align-items: stretch;
          }
      </style>
-     <div class="container d-none d-lg-block mb-3">
-         <div class="row">
-             <div class="col-wd-3 col-lg-4">
-                 <div class="widget-column">
-                     <div class="border-bottom border-color-1 mb-5">
-                         <h3 class="section-title section-title__sm mb-0 pb-2 font-size-18">Sản phẩm nổi bật</h3>
-                     </div>
-                     <ul class="list-unstyled products-group">
-                         @foreach ($topViewedProducts as $product)
-                             <li class="product-item product-item__list row no-gutters mb-6 remove-divider">
-                                 <div class="col-auto">
-                                     <a href="{{ route('client.products.detail', $product->slug) }}"
-                                         class="d-block width-75 text-center">
-                                         <img class="img-fluid" src="{{ asset('storage/' . $product->thumbnail) }}"
-                                             alt="{{ $product->name }}">
-                                     </a>
-                                 </div>
-                                 <div class="col pl-4 d-flex flex-column">
-                                     <h5 class="product-item__title mb-0">
-                                         <a href="{{ route('client.products.detail', $product->slug) }}"
-                                             class="text-blue font-weight-bold">
-                                             {{ $product->name }}
-                                         </a>
-                                     </h5>
-                                     <div class="prodcut-price mt-auto">
-                                         @if ($product->price_sale && $product->price_sale < $product->price)
-                                             <div class="d-flex align-items-center">
-                                                 <span class="font-size-15 text-danger font-weight-bold">
-                                                     {{ number_format($product->price_sale, 0, ',', '.') }}₫
-                                                 </span>
-                                                 <span class="font-size-13 text-muted ml-2"
-                                                     style="text-decoration: line-through;">
-                                                     {{ number_format($product->price, 0, ',', '.') }}₫
-                                                 </span>
-                                             </div>
-                                         @else
-                                             <div class="font-size-15">
-                                                 {{ number_format($product->price, 0, ',', '.') }}₫
-                                             </div>
-                                         @endif
-                                     </div>
-
-                                 </div>
-                             </li>
-                         @endforeach
-                     </ul>
-                 </div>
-             </div>
-
-             <div class="col-wd-3 col-lg-4">
-                 <div class="border-bottom border-color-1 mb-5">
-                     <h3 class="section-title section-title__sm mb-0 pb-2 font-size-18">Sản phẩm đang bán</h3>
-                 </div>
-                 <ul class="list-unstyled products-group">
-                     @foreach ($activeProducts as $product)
-                         <li class="product-item product-item__list row no-gutters mb-6 remove-divider">
-                             <div class="col-auto">
-                                 <a href="{{ route('client.products.detail', $product->slug) }}"
-                                     class="d-block width-75 text-center">
-                                     <img class="img-fluid" src="{{ asset('storage/' . $product->thumbnail) }}"
-                                         alt="{{ $product->name }}">
-                                 </a>
-                             </div>
-                             <div class="col pl-4 d-flex flex-column">
-                                 <h5 class="product-item__title mb-0">
-                                     <a href="{{ route('client.products.detail', $product->slug) }}"
-                                         class="text-blue font-weight-bold">
-                                         {{ $product->name }}
-                                     </a>
-                                 </h5>
-                                 <div class="prodcut-price mt-auto">
-                                     @if ($product->price_sale && $product->price_sale < $product->price)
-                                         <div class="d-flex align-items-center">
-                                             <span class="font-size-15 text-danger font-weight-bold">
-                                                 {{ number_format($product->price_sale, 0, ',', '.') }}₫
-                                             </span>
-                                             <span class="font-size-13 text-muted ml-2"
-                                                 style="text-decoration: line-through;">
-                                                 {{ number_format($product->price, 0, ',', '.') }}₫
-                                             </span>
-                                         </div>
-                                     @else
-                                         <div class="font-size-15">
-                                             {{ number_format($product->price, 0, ',', '.') }}₫
-                                         </div>
-                                     @endif
-                                 </div>
-
-                             </div>
-                         </li>
-                     @endforeach
-                 </ul>
-             </div>
-             <div class="col-wd-3 col-lg-4">
-                 <div class="border-bottom border-color-1 mb-5">
-                     <h3 class="section-title section-title__sm mb-0 pb-2 font-size-18">Sản phẩm được đánh giá cao nhất
-                     </h3>
-                 </div>
-                 <ul class="list-unstyled products-group">
-                     @foreach ($topRatedProducts as $product)
-                         <li class="product-item product-item__list row no-gutters mb-6 remove-divider">
-                             <div class="col-auto">
-                                 <a href="{{ route('client.products.detail', $product->slug) }}"
-                                     class="d-block width-75 text-center">
-                                     <img class="img-fluid" src="{{ asset('storage/' . $product->thumbnail) }}"
-                                         alt="{{ $product->name }}">
-                                 </a>
-                             </div>
-                             <div class="col pl-4 d-flex flex-column">
-                                 <h5 class="product-item__title mb-0">
-                                     <a href="{{ route('client.products.detail', $product->slug) }}"
-                                         class="text-blue font-weight-bold">
-                                         {{ $product->name }}
-                                     </a>
-                                 </h5>
-                                 <div class="text-warning mb-2">
-                                     @for ($i = 0; $i < floor($product->comments_avg_rating); $i++)
-                                         <small class="fas fa-star"></small> <!-- Full star -->
-                                     @endfor
-                                     @for ($i = 0; $i < 5 - floor($product->comments_avg_rating); $i++)
-                                         <small class="far fa-star text-muted"></small> <!-- Empty star -->
-                                     @endfor
-                                 </div>
-                                 <div class="prodcut-price mt-auto">
-                                     @if ($product->price_sale && $product->price_sale < $product->price)
-                                         <div class="d-flex align-items-center">
-                                             <span class="font-size-15 text-danger font-weight-bold">
-                                                 {{ number_format($product->price_sale, 0, ',', '.') }}₫
-                                             </span>
-                                             <span class="font-size-13 text-muted ml-2"
-                                                 style="text-decoration: line-through;">
-                                                 {{ number_format($product->price, 0, ',', '.') }}₫
-                                             </span>
-                                         </div>
-                                     @else
-                                         <div class="font-size-15">
-                                             {{ number_format($product->price, 0, ',', '.') }}₫
-                                         </div>
-                                     @endif
-                                 </div>
-                             </div>
-                         </li>
-                     @endforeach
-                 </ul>
-
-             </div>
-             <div class="col-wd-3 d-none d-wd-block">
-                 <a href="../shop/shop.html" class="d-block"><img class="img-fluid"
-                         src="fontend/assets/img/330X360/img1.jpg" alt="Image Description"></a>
-             </div>
-         </div>
-     </div>
      <!-- Footer-newsletter -->
      <div class="bg-primary py-3">
          <div class="container">
@@ -286,27 +134,23 @@
                      <div class="row align-items-stretch">
                          <!-- Tìm kiếm nhanh -->
                          <div class="col-12 col-md mb-4 mb-md-0 d-flex flex-column h-100">
-                             <h6 class="mb-3 font-weight-bold">Tìm kiếm nhanh</h6>
+                             <h6 class="mb-3 font-weight-bold text-uppercase">Sản phẩm máy tính</h6>
                              <ul class="list-group list-group-flush list-group-borderless mb-0 list-group-transparent">
                                  <li><a class="list-group-item list-group-item-action"
-                                         href="{{ route('client.products.index', ['category' => 'laptop']) }}">Laptop
-                                         & Máy tính</a></li>
-                                 {{-- <li><a class="list-group-item list-group-item-action" href="{{ route('client.products.index', ['category' => 'camera']) }}">Máy ảnh & Nhiếp ảnh</a></li> --}}
+                                         href="{{ route('client.products.index', ['category' => 'laptop']) }}">Laptop &
+                                         Máy tính xách tay</a></li>
                                  <li><a class="list-group-item list-group-item-action"
-                                         href="{{ route('client.products.index', ['category' => 'smartphone']) }}">Máy
-                                         tính bảng</a></li>
+                                         href="{{ route('client.products.index', ['category' => 'desktop']) }}">Máy tính
+                                         để bàn (Desktop)</a></li>
                                  <li><a class="list-group-item list-group-item-action"
-                                         href="{{ route('client.products.index', ['category' => 'gaming']) }}">Game
-                                         & Máy chơi game</a></li>
+                                         href="{{ route('client.products.index', ['category' => 'gaming']) }}">Máy tính
+                                         chơi game</a></li>
                                  <li><a class="list-group-item list-group-item-action"
-                                         href="{{ route('client.products.index', ['category' => 'tv-audio']) }}">TV
-                                         & Âm thanh</a></li>
+                                         href="{{ route('client.products.index', ['category' => 'all-in-one']) }}">Máy
+                                         tính All-in-One</a></li>
                                  <li><a class="list-group-item list-group-item-action"
-                                         href="{{ route('client.products.index', ['category' => 'smart-devices']) }}">Thiết
-                                         bị thông minh</a></li>
-                                 <li><a class="list-group-item list-group-item-action"
-                                         href="{{ route('client.products.index', ['category' => 'car-electronics']) }}">Thiết
-                                         bị xe hơi & GPS</a></li>
+                                         href="{{ route('client.products.index', ['category' => 'workstation']) }}">Máy
+                                         trạm Workstation</a></li>
                              </ul>
                          </div>
 
@@ -314,15 +158,7 @@
                          <div class="col-12 col-md mb-4 mb-md-0 d-flex flex-column h-100">
                              <h6 class="mb-3 font-weight-bold">Danh mục thiết bị - phụ kiện</h6>
                              <ul class="list-group list-group-flush list-group-borderless mb-0 list-group-transparent">
-                                 <li><a class="list-group-item list-group-item-action"
-                                         href="{{ route('client.products.index', ['category' => 'printer']) }}">Máy
-                                         in & Mực in</a></li>
-                                 <li><a class="list-group-item list-group-item-action"
-                                         href="{{ route('client.products.index', ['category' => 'software']) }}">Phần
-                                         mềm</a></li>
-                                 <li><a class="list-group-item list-group-item-action"
-                                         href="{{ route('client.products.index', ['category' => 'stationery']) }}">Văn
-                                         phòng phẩm</a></li>
+
                                  <li><a class="list-group-item list-group-item-action"
                                          href="{{ route('client.products.index', ['category' => 'components']) }}">Linh
                                          kiện máy tính</a></li>
