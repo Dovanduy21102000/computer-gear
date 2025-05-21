1b4659dddfb43ad8b34e19f900dd2dcc56892b99
LÊ NGOC MINH <minhle1309@gmail.com>
2025-05-21 09:35:06 +0700

    căn lại thẳng dòng cho phần droplist ở topbar

--- resources/views/fontend/component/topbar.blade.php
+++ resources/views/fontend/component/topbar.blade.php
@@ -17,39 +17,31 @@
                                 class="ec ec-transport mr-1"></i> Theo dõi đơn hàng</a>
                     </li>
 
-                    <li class="list-inline-item u-header-topbar__nav-item u-header-topbar__nav-item-border">
-                        <a id="sidebarNavToggler" href="javascript:;" class="u-header-topbar__nav-link">
-                            @auth
-                                <div class="dropdown">
-                                    <a id="userDropdown" href="javascript:;"
-                                        class="dropdown-toggle d-flex align-items-center text-dark" role="button"
-                                        data-toggle="dropdown">
-                                        <i class="ec ec-user mr-1"></i> Xin chào, {{ Auth::user()->name }}
-                                    </a>
-
-                                    <div class="dropdown-menu dropdown-menu-right">
-                                        <a class="dropdown-item" href="{{ route('user.show') }}">Quản lý tài khoản</a>
-                                        <a class="dropdown-item" href="{{ route('client.orders.index') }}">Quản lý đơn
-                                            hàng</a>
-                                        <a class="dropdown-item" href="{{ route('payment.resume.index') }}">Thanh toán đang
-                                            chờ</a>
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                            Đăng xuất
-                                        </a>
-                                    </div>
-                                </div>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            @else
-                                <div>
-                                    <i class="ec ec-user mr-1"></i>
-                                    <a class="text-dark" href="{{ route('login.form') }}">Đăng nhập</a>
-                                </div>
-
-                            @endauth
-                        </a>
+                    <li
+                        class="list-inline-item u-header-topbar__nav-item u-header-topbar__nav-item-border align-items-center">
+                        @auth
+                            <a id="userDropdown" href="javascript:;"
+                                class="dropdown-toggle text-dark u-header-topbar__nav-link" role="button"
+                                data-toggle="dropdown">
+                                <i class="ec ec-user mr-1"></i> Xin chào, {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <a class="dropdown-item" href="{{ route('user.show') }}">Quản lý tài khoản</a>
+                                <a class="dropdown-item" href="{{ route('client.orders.index') }}">Quản lý đơn hàng</a>
+                                <a class="dropdown-item" href="{{ route('payment.resume.index') }}">Thanh toán đang chờ</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    Đăng xuất
+                                </a>
+                            </div>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        @else
+                            <a class="u-header-topbar__nav-link text-dark" href="{{ route('login.form') }}">
+                                <i class="ec ec-user mr-1"></i> Đăng nhập
+                            </a>
+                        @endauth
                     </li>
                 </ul>
             </div>
