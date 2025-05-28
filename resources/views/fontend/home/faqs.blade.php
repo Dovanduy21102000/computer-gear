bf358cc2b8f2dc48a556eeb5b6c6059e93167872
truongxim1806 <truongxim1806@gmail.com>
2025-05-28 23:23:32 +0700

    fix lại giao diện

--- resources/views/fontend/home/faqs.blade.php
+++ resources/views/fontend/home/faqs.blade.php
@@ -22,7 +22,25 @@
             <h1>Câu Hỏi Thường Gặp</h1>
             <p class="text-gray-44">Thông tin được cập nhật lần cuối vào ngày 28 tháng 05 năm 2025</p>
         </div>
-        
+        <div class="mb-12 text-center">
+            <h1>Câu Hỏi Thường Gặp</h1>
+            <p class="text-gray-44">Thông tin được cập nhật lần cuối vào ngày 28 tháng 05 năm 2025</p>
+        </div>
+        <div class="border-bottom border-color-1 mb-8 rounded-0">
+            <h3 class="section-title mb-0 pb-2 font-size-25">Thông Tin Giao Hàng</h3>
+        </div>
+        <div class="row mb-8">
+            <div class="col-lg-6 mb-5 mb-lg-8">
+                <h3 class="font-size-18 font-weight-semi-bold text-gray-39 mb-4">Có những phương thức giao hàng nào?</h3>
+                <p class="text-gray-90">Chúng tôi hỗ trợ giao hàng tiêu chuẩn và giao hàng nhanh thông qua các đơn vị vận chuyển
+                    uy tín như Viettel Post, GHN và GHTK.</p>
+            </div>
+            <div class="col-lg-6 mb-5 mb-lg-8">
+                <h3 class="font-size-18 font-weight-semi-bold text-gray-39 mb-4">Thời gian nhận hàng là bao lâu?</h3>
+                <p class="text-gray-90">Thời gian giao hàng tuỳ thuộc vào khu vực giao hàng, thời gian có thể từ 1-3 ngày làm
+                    việc đối với nội thành, và 3-7 ngày làm việc đối với khu vực ngoại thành hoặc tỉnh thành khác.</p>
+            </div>
+
         <!-- End Basics Accordion -->
     </div>
 </main>
