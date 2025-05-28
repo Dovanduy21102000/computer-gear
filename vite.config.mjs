969e4c205a4d10bc56ca62b94630ffedc21e4750
ndh0104 <ndh012004@gmail.com>
2025-05-28 17:38:58 +0700

    Fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,12 +3,12 @@ import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     server: {
-        host: "computer-gear.test", // Use your custom domain
+        host: "computer-gear.com", // Use your custom domain
         port: 5173, // Default Vite port
         strictPort: true,
         cors: true,
         hmr: {
-            host: "computer-gear.test", // HMR host must match your domain
+            host: "computer-gear.com", // HMR host must match your domain
         },
     },
     plugins: [
